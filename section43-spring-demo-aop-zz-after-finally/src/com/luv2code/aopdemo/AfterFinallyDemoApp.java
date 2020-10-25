668422e4a19e19a6414593e022b7cd48197e33fc
annaperev <perev13@gmail.com>
2020-10-25 19:01:06 +0300

    @After(finally)

---
+++ section43-spring-demo-aop-zz-after-finally/src/com/luv2code/aopdemo/AfterFinallyDemoApp.java
@@ -0,0 +1,40 @@
+package com.luv2code.aopdemo;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class AfterFinallyDemoApp {
+    public static void main(String[] args) {
+
+        // read spring config java class
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        // get AccountDAO bean from spring container
+        AccountDAO accountDAO = context.getBean("accountDAO",AccountDAO.class);
+
+        // call method to find the accounts
+        List<Account> theAccounts = null;
+        try{
+            // flag to simulate exception
+            boolean tripWire = true;
+            theAccounts = accountDAO.findAccounts(tripWire);
+        }
+        catch (Exception e){
+            System.out.println("\n\nMain Program ... caught exception: " + e);
+        }
+
+        // display the accounts
+        System.out.println("\n\nMain Program: AfterFinallyDemoApp");
+        System.out.println("----");
+
+        System.out.println(theAccounts);
+
+        System.out.println("\n");
+
+        // close the context
+        context.close();
+    }
+}
