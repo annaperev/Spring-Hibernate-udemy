71525601d6bdfadd230283de07349a9c760b24da
annaperev <perev13@gmail.com>
2020-10-25 20:10:48 +0300

    @Around handle exception

---
+++ section44-spring-demo-aop-zzz-around-handle-exception/src/com/luv2code/aopdemo/AroundHandleExceptionDemoApp.java
@@ -0,0 +1,34 @@
+package com.luv2code.aopdemo;
+
+import com.luv2code.aopdemo.service.TrafficFortuneService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.logging.Logger;
+
+public class AroundHandleExceptionDemoApp {
+
+    // 355
+    private static Logger myLogger =
+            Logger.getLogger(AroundHandleExceptionDemoApp.class.getName());
+
+    public static void main(String[] args) {
+
+        // read spring config java class
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        // get TrafficFortuneService bean from spring container
+        TrafficFortuneService fortuneService = context.getBean("trafficFortuneService",TrafficFortuneService.class);
+
+        myLogger.info("\nMain Program: AroundDemoApp");
+
+        myLogger.info("Calling getFortune");
+
+        boolean tripWire = true;
+        String data = fortuneService.getFortune(tripWire);
+
+        myLogger.info("\nMy Fortune is: " + data);
+        // close the context
+        context.close();
+    }
+}
