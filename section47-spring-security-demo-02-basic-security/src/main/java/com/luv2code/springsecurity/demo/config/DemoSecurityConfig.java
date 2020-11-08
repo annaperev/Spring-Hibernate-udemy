477c7890611e9863fd2141149f94cb1bb3071498
annaperev <perev13@gmail.com>
2020-11-08 16:20:08 +0300

    spring security custom login form

--- section47-spring-security-demo-02-basic-security/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ section47-spring-security-demo-02-basic-security/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.luv2code.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
---
+++ section48-spring-security-demo-03-custom-login-form/target/ssection48-spring-security-demo-03-custom-login-form/WEB-INF/view/home.jsp
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+    <title>Anna Company home page</title>
+</head>
+<body>
+    <h2>Anna Company Home Page - Yoohoo</h2>
+    <hr>
+
+    Welcome to Anna company home page
+</body>
+</html>
